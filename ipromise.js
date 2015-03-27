279edcd2f86639fa208c7da8a6fd03fe10134157
godsong <gaosong@hitour.cc>
2015-03-27 18:09:07 +0800

    complete

--- ipromise.js
+++ ipromise.js
@@ -2,190 +2,176 @@
  * A link-style promise Implementation
  */
 var _Slice = Array.prototype.slice;
-var _when = function() {
-    var count = arguments.length;
-    var args = [], p = this;
 
-    function sandGlass() {//计时器 作为when里所有promise的then 每个promise 完成时 count递减 当count为0时代表when里所有的promise都完成了 此时resolve when这个promise
+var _safeWriteFlag = false;
+var STATE = 1, VALUE = 2;
+window.IPromise = function(func) {
+    return new Promise(func);
+};
+function all(){
+    var p=new Promise(),count=arguments.length,data=[];
+    function sandGlass(v){
         count--;
-        args[this.params.index] = arguments.length==1?arguments[0]:_Slice.call(arguments);
-        if(count == 0) {
-            p.resolve.apply(p, args);
+        data[this.idx]=v;
+        if(count==0){
+            p.resolve(data);
         }
     }
-
-    for(var i = 0; i < arguments.length; i++) {
-        var pro = arguments[i];
-        if(typeof pro === 'function') {
-            var ret = pro();
-            if(ret instanceof Promise) {
-                ret.then(sandGlass);
-                ret.params.index=i;
-            }
-            else {
-                count--;
-                args[i] = ret;
-            }
-        }
-        else if(pro instanceof Promise){
+    for(var i=0;i<arguments.length;i++){
+        var pro=arguments[i];
+        if(pro instanceof Promise){
+            pro.idx=i;
             pro.then(sandGlass);
-            pro.params.index=i;
         }
-        else {
-            args[i] = pro;
-            count--;
-        }
-    }
-    if(count==0){
-        p.resolve.apply(p, args);
-    }
-    return p;
-};
-var _any = function() {
-
-    var args = [], p = this;
-
-    function sandGlass() {//计时器 作为when里所有promise的then 每个promise 完成时 count递减 当count为0时代表when里所有的promise都完成了 此时resolve when这个promise
-        if(p.state == 'pending') {
-
-            p.resolve.apply(p, args);
-        }
-    }
-
-    for(var i = 0; i < arguments.length; i++) {
-        var pro = arguments[i];
-        if(typeof pro === 'function') {
-            var ret = pro();
-            if(ret instanceof Promise) {
-                ret.then(sandGlass);
-                ret.index = i;
-            }
-            else {
-                count--;
-                args[i] = ret;
-            }
-        }
-        else {
-            args[i] = pro;
+        else{
+            data[i]=pro;
             count--;
         }
     }
     return p;
-
-};
-var _cache = {}, _uuid = 0;
-
-var Promise = module.exports = function Promise(func) {
-    this.state = 'pending';
-    this.params={};
-    if(typeof func === 'function') {
-        var p = this;
-        func(function() {
-            p.resolve.apply(p, _Slice.call(arguments));
-        }, function() {
-            p.reject.apply(p, _Slice.call(arguments));
-        })
+}
+function race(){
+    var p=new Promise();
+    function sandGlass(v){
+        p.resolve(v);
     }
-    this.uuid = _uuid++;
+    for(var i=0;i<arguments.length;i++){
+        var pro=arguments[i];
+        if(pro instanceof Promise){
+            pro.then(sandGlass);
+        }
+    }
+    return p;
+}
+function Promise() {
+    var state = 'pending', value = null;
+    this.get = function(prop) {
+        if(prop === STATE) {
+            return state;
+        }
+        else if(prop === VALUE) {
+            return value;
+        }
+
+    };
+    this.set = function(prop, v) {
+        if(_safeWriteFlag) {
+            if(prop === STATE) {
+                state = value;
+            }
+            else if(prop === VALUE) {
+                value = v;
+            }
+        }
+    };
+    this.onFulfilledQueue = [];//Promise/A+ 2.2.6对于同一个promise then可能会调用多次 因此需要一个回调列表
+    this.onRejectedQueue = [];
+    this.onProgressList = [];
+    this.params = {};
 }
 
 Promise.prototype = {
-    then      : function(onFulfilled, onRejected) {
-        this.onFulfilled = onFulfilled;
-        this.onRejected = onRejected;
-        this.next = new Promise();//生成新的Promise 此Promise实际上是对可能的后续异步函数结果的一个保证
-        if(this.state == 'fulfilled') {
-            this.resolve.apply(this, _cache[this.uuid]);
-        }
-        if(this.state == 'reject') {
-            this.reject.apply(this, _cache[this.uuid]);
-        }
-        return this.next;
-    },
-    resolve   : function() {
-        if(this.next) {
-            if(this.onFulfilled) {
-                this.state = 'fulfilled';
-                var ret = this.onFulfilled.apply(this, _Slice.call(arguments));
-                if(ret instanceof Promise) {
-                    //移花接木 将当前promise onFulfilled函数中生成的promise转化成当前的后续promise(该后续promise本身就是为它预生成的)
-                    ret.onFulfilled = this.next.onFulfilled;
-                    ret.onRejected = this.next.onRejected;
-                    ret.uuid = this.next.uuid;
-                    ret.next = this.next.next;
-                    ret.params=this.next.params;
-                    if(ret.next == null) {
-                        //借尸还魂 当前promise的onFulfilled函数已经是最后一个函数 生成的promise无法被用户引用，因此需要把fulfill状态置回为它预生成的那个promise上一遍后续的then
-                        ret.proto = this.next;
-                    }
-                }
-                else {
-                    var p = new Promise();
-                    p.onFulfilled = this.next.onFulfilled;
-                    p.onRejected = this.next.onRejected;
-                    p.uuid = this.next.uuid;
-                    p.next = this.next.next;
-                    p.params=this.next.params;
-                    if(p.next == null) {
-                        p.proto = this.next;
-                    }
-                    if(ret) {
-                        p.resolve(ret);
-                    }
-                    else{
-                        p.resolve.apply(p, _Slice.call(arguments));
-                    }
-                }
-            }
-            else {
-                this.next.resolve();
-            }
-        }
-        else {
-            if(this.proto) {
-                this.proto.state = 'fulfilled';
-                _cache[this.proto.uuid] = _Slice.call(arguments);
-            }
-            else {
-                this.state = 'fulfilled';
-                _cache[this.uuid] = _Slice.call(arguments);
-            }
 
-        }
-        return this;
-    },
-    reject    : function() {
-        this.state = 'reject';
-        if(this.onRejected) {
-            this.onRejected.apply(this, _Slice.call(arguments));
-        }
-        else {
-            if(this.next) {
-                this.next.reject.apply(this.next, _Slice.call(arguments));//错误冒泡
-            }
-            else {
-                _cache[this.uuid] = _Slice.call(arguments);
-            }
+    then    : function(onFulfilled, onRejected, onProgress) {
 
+        var next = new Promise();
+        if(typeof onFulfilled === 'function') {//Promise/A+ 2.2.1 非函数的参数将被忽略
+            this.onFulfilledQueue.push({next : next, onFulfilled : onFulfilled});
         }
+        if(typeof onRejected === 'function') {
+            this.onRejectedQueue.push({next : next, onRejected : onRejected});
+        }
+        if(typeof onProgress === 'function') {
+            //此处为附加的功能 规范中并未规定 指onFulfilled执行前所触发的回调
+            this.onProgressList.push(onProgress);
+        }
+        this.next = next;
+        if(this.get(STATE) == 'fulfilled') {
+            this.resolve.call(this, this.get(VALUE));
+        }
+        if(this.get(STATE) == 'rejected') {
+            this.reject.call(this, this.get(VALUE));
+        }
+        return next;
     },
-    when      : function() {
-        _when.apply(this, _Slice.call(arguments));
-        return this;
-    },
-    crossFire : function() {
+    resolve : function(value) {
+        if(this.get(STATE)=='pending') {
+            _resolve(this,value);
+        }
 
     },
-    value     : function(n) {
-        if(typeof n == 'number') {
-            return _cache[this.uuid][n];
+    reject  : function(value) {
+        if(this.get(STATE)=='pending') {
+            _onStateChange(this, 'rejected', value);
         }
-        return _cache[this.uuid];
+    },
+    catch:function(onRejected){
+        return this.then(null,onRejected);
+    },
+    finally:function(onFinally){
+        this.onFinally=onFinally;
     }
 };
+Promise.race=race;
+Promise.all=all;
+function _resolve(promise,value){
+    if(value instanceof Promise) {
+        value.then(function promiseResolve(v) {
+            promise.resolve(v);
+        }, function promiseReject(r) {
+            promise.reject(r);
+        });
 
-Promise.when = function() {
-    var p = new Promise();
-    _when.apply(p, _Slice.call(arguments));
-    return p;
-};
+    }
+    else {
+        _onStateChange(promise, 'fulfilled', value);
+    }
+    value=null;
+}
+function _onStateChange(promise, state, value) {
+    _safeWriteFlag = true;
+    promise.set(STATE, state);
+    promise.set(VALUE, value);
+    _safeWriteFlag = false;
+    var queue;
+    if(state === 'fulfilled') {
+        if(promise.onFulfilledQueue.length > 0) {
+            while(queue = promise.onFulfilledQueue.shift()) {
+                try {
+                    var ret = queue.onFulfilled.call(promise,value);
+                    queue.next.resolve(ret);
+                } catch(exception) {
+                    console.error(exception);
+                    console.log(exception.stack);
+                    queue.next.reject(exception);
+                }
+
+            }
+        } else {
+            promise.next && promise.next.resolve.call(promise,value);
+        }
+    }
+    else if(state === 'rejected') {
+        if(promise.onRejectedQueue.length > 0) {
+            while(queue = promise.onRejectedQueue.shift()) {
+                try {
+                    ret = queue.onRejected.call(promise,value);
+                    if(ret === undefined) {//继续异常冒泡
+                        queue.next.reject(value);
+                    }
+                    else if(ret !== 'false') {//异常恢复
+                        queue.next.resolve(ret)
+                    }
+                    //else if(ret===false) //停止异常冒泡
+                } catch(exception) {
+                    console.error(exception);
+                    queue.next.reject(exception);
+                }
+
+            }
+        } else {
+            promise.next && promise.next.reject.call(promise,value);
+        }
+    }
+    promise.onFinally&&promise.onFinally.call(promise,value)
+}
